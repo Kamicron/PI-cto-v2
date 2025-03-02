cc0afa48448113ec5ee40bda68accfd92b08e8f9
Romin Manogil <romin@valuemycar.fr>
2025-03-02 18:30:33 +0100

    dev: added component & list of folders

--- frontend/pages/test.vue
+++ frontend/pages/test.vue
@@ -1,70 +1,26 @@
 <template>
-  <button @click="showToastSuccess">Show Toast Success</button>
-  <button @click="showToastError">Show Toast Error</button>
-  <button @click="showToastWarning">Show Toast Warning</button>
-  <button @click="showToastInfo">Show Toast Info</button>
-
+  <div class="wrapper">
+    <folders-list :folders="folders"/>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { EToast } from 'vue3-modern-toast'
+const { $api } = useNuxtApp()
 
-const { $toast } = useNuxtApp()
+const folders = ref<any[]>([])
+const openFolders = ref(new Set<number>())
 
-function showToastSuccess() {
-  $toast.show({
-    message: 'Success message',
-    type: EToast.SUCCESS,
-    duration: 3000,
-    dismissible: true,
-    icon: '✨'
-  })
+onMounted(() => getAllFolders())
+
+async function getAllFolders() {
+  const { data, error } = await $api.get('/folders')
+  if (!data || error) return
+
+  folders.value = data.map(folder => ({
+    ...folder,
+    children: folder.children || [] 
+  }))
 }
-
-function showToastError() {
-  $toast.show({
-    message: 'Error message',
-    type: EToast.ERROR,
-    duration: 3000,
-    dismissible: true,
-    icon: '⛔'
-  })
-}
-
-function showToastWarning() {
-  $toast.show({
-    message: 'Warning message',
-    type: EToast.WARNING,
-    duration: 3000,
-    dismissible: true,
-    icon: '⚠'
-  })
-}
-
-function showToastInfo() {
-  $toast.show({
-    message: 'Info message',
-    type: EToast.INFO,
-    duration: 3000,
-    dismissible: true,
-    icon: ''
-  })
-}
-
-
 </script>
 
-<style lang='scss' scoped>
-button {
-  padding: 10px 20px;
-  background-color: #4299E1;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: #2B6CB0;
-  }
-}
-</style>
+<style lang='scss' scoped></style>
